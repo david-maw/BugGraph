786129285d8f641f3f0f93b8e2ceb3c26a92780c
David Maw <david@maw.name>
2022-11-05 19:37:44 -0700

    Add Features
    Token persistence between Runs on Windows
    
    Default authority so as to allow OneDrive file access

--- AppConstants.cs
+++ AppConstants.cs
@@ -4,11 +4,8 @@ public static class AppConstants
     // ClientID of the application in (ms sample testing)
     public const string ClientId = "858b4a09-dc31-45d3-83a7-2b5f024f99cd"; // TODO - Replace with your client Id. And also replace in the AndroidManifest.xml
 
-    // TenantID of the organization (ms sample testing)
-    public const string TenantId = "7f58f645-c190-4ce5-9de4-e2b7acd2a6ab"; // TODO - Replace with your TenantID. And also replace in the AndroidManifest.xml
-
     /// <summary>
     /// Scopes defining what app can access in the graph
     /// </summary>
-    public static string[] Scopes = { "User.Read" };
+    public static string[] Scopes = { "User.Read", "MailboxSettings.Read", "Files.ReadWrite.All" };
 }
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,96 +1,124 @@
 ﻿using Microsoft.Graph.Models;
 using Microsoft.Identity.Client;
 using System.Diagnostics;
-
 using Azure.Identity;
 using Microsoft.Kiota.Abstractions.Authentication;
 using Microsoft.Graph;
+using Microsoft.Identity.Client.Extensions.Msal;
 
 namespace Graph6;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    int count = 0;
     private User user;
     public MainPage()
-	{
-		InitializeComponent();
-	}
+    {
+        InitializeComponent();
+    }
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
     private async void GetUserInfoBtn_Clicked(object sender, EventArgs e)
     {
 #if false
-		// using Azure.Identity; 
-		// does not work on Android
-		var options = new InteractiveBrowserCredentialOptions
-		{
-			TenantId = AppConstants.TenantId,
-			ClientId = AppConstants.ClientId,
-			AuthorityHost = AzureAuthorityHosts.AzurePublicCloud,
-			// MUST be http://localhost or http://localhost:PORT
-			// See https://github.com/AzureAD/microsoft-authentication-library-for-dotnet/wiki/System-Browser-on-.Net-Core
-			RedirectUri = new Uri("http://localhost"),
-		};
+        // using Azure.Identity; 
+        // does not work on Android
+        var options = new InteractiveBrowserCredentialOptions
+        {
+            TenantId = AppConstants.TenantId,
+            ClientId = AppConstants.ClientId,
+            AuthorityHost = AzureAuthorityHosts.AzurePublicCloud,
+            // MUST be http://localhost or http://localhost:PORT
+            // See https://github.com/AzureAD/microsoft-authentication-library-for-dotnet/wiki/System-Browser-on-.Net-Core
+            RedirectUri = new Uri("http://localhost"),
+        };
 
-		// https://learn.microsoft.com/dotnet/api/azure.identity.interactivebrowsercredential
-		var interactiveCredential = new InteractiveBrowserCredential(options);
+        // https://learn.microsoft.com/dotnet/api/azure.identity.interactivebrowsercredential
+        var interactiveCredential = new InteractiveBrowserCredential(options);
 
-		var graphClient = new GraphServiceClient(interactiveCredential, AppConstants.Scopes);
+        var graphClient = new GraphServiceClient(interactiveCredential, AppConstants.Scopes);
 #endif
 
         var authenticationProvider = new BaseBearerTokenAuthenticationProvider(new TokenProvider());
         var graphClient = new GraphServiceClient(authenticationProvider);
 
         try
-		{
+        {
             user = await graphClient.Me.GetAsync();
+            var x = await graphClient.Me.Drive.GetAsync(); // graphClient.Me.Drive.Items.GetAsync(); does not seem to compile
+            Debug.WriteLine(">>> graphClient returned " + x.GetType());
         }
-		catch (Exception ex)
-		{
-			Debug.WriteLine("Exception:" + ex);
-		}
+        catch (Exception ex)
+        {
+            Debug.WriteLine("Exception:" + ex);
+        }
 
-		if (user == null)
-			HelloLabel.Text = "User information could not be found";
-		else
-			HelloLabel.Text = $"Hello, {user.DisplayName}!";
+        if (user == null)
+            HelloLabel.Text = "User information could not be found";
+        else
+            HelloLabel.Text = $"Hello, {user.DisplayName}!";
     }
 
     public class TokenProvider : IAccessTokenProvider
     {
 
-		// Using Microsoft.Identity.Client
-		private readonly IPublicClientApplication publicClientApplication;
+        // Using Microsoft.Identity.Client
+        private readonly IPublicClientApplication publicClientApplication;
 
-		public TokenProvider()
-		{
-			publicClientApplication = PublicClientApplicationBuilder.Create(AppConstants.ClientId)
-                .WithAuthority($"https://login.microsoftonline.com/{AppConstants.TenantId}/")
+        public TokenProvider()
+        {
+            publicClientApplication = PublicClientApplicationBuilder.Create(AppConstants.ClientId)
                 .WithDefaultRedirectUri()
                 .Build();
         }
 
-        public async Task<string> GetAuthorizationTokenAsync(Uri uri, Dictionary<string, object> additionalAuthenticationContext = default,CancellationToken cancellationToken = default)
-        {
-            var authenticationResult = await publicClientApplication.AcquireTokenInteractive(AppConstants.Scopes)
-#if ANDROID
-							.WithParentActivityOrWindow(Platform.CurrentActivity)
+#if WINDOWS
+        private bool once = true;
 #endif
-							.ExecuteAsync();
+        public async Task<string> GetAuthorizationTokenAsync(Uri uri, Dictionary<string, object> additionalAuthenticationContext = default, CancellationToken cancellationToken = default)
+        {
+#if WINDOWS
+            if (once)
+            {
+                once = false;
+                var storageProperties =
+                     new StorageCreationPropertiesBuilder(@"TokenCache", @"C:\Temp")
+                     .Build();
 
-            // get the token and return it in your own way
-            return authenticationResult.AccessToken;
+                // This hooks up the cross-platform cache into MSAL
+                var cacheHelper = await MsalCacheHelper.CreateAsync(storageProperties);
+                cacheHelper.RegisterCache(publicClientApplication.UserTokenCache);
+            }
+#endif
+            try
+            {
+                var accounts = await publicClientApplication.GetAccountsAsync();
+                var authenticationResult = await publicClientApplication.AcquireTokenSilent(AppConstants.Scopes, accounts.FirstOrDefault()).ExecuteAsync();
+
+                // get the token and return it in your own way
+                return authenticationResult.AccessToken;
+            }
+            catch (MsalUiRequiredException)
+            {
+                var authenticationResult = await publicClientApplication.AcquireTokenInteractive(AppConstants.Scopes)
+#if ANDROID
+                            .WithParentActivityOrWindow(Platform.CurrentActivity)
+#endif
+                            .ExecuteAsync();
+
+                // get the token and return it in your own way
+                return authenticationResult.AccessToken;
+            }
         }
 
         public AllowedHostsValidator AllowedHostsValidator { get; }
